f3f902942d8c93c9f36b53673e4e5923dfab4a1a
amolbudkhe <abudkhe>
2023-06-21 12:49:04 +0530

    added other way of routes commented

--- src/App.js
+++ src/App.js
@@ -7,19 +7,19 @@ import {
 import HomePage from "./pages/HomePage";
 import ProductsPage from "./pages/ProductsPage";
 
-const routeDefinations = createRoutesFromElements(
-  <Route>
-    <Route path="/" element={<HomePage />}></Route>
-    <Route path="/products" element={<ProductsPage />}></Route>
-  </Route>
-);
+// const routeDefinations = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage />}></Route>
+//     <Route path="/products" element={<ProductsPage />}></Route>
+//   </Route>
+// );
 
-// const router = createBrowserRouter([
-//   { path: "/", element: <HomePage /> },
-//   { path: "/products", element: <ProductsPage /> },
-// ]);
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  { path: "/products", element: <ProductsPage /> },
+]);
 
-const router = createBrowserRouter(routeDefinations);
+// const router = createBrowserRouter(routeDefinations);
 
 function App() {
   return <RouterProvider router={router} />;
