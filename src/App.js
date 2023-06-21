a09ecc054375ff72850176e5e4a351f2f44ad602
amolbudkhe <abudkhe>
2023-06-21 12:42:35 +0530

    added home and product page routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ProductsPage from "./pages/ProductsPage";
+
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  { path: "/products", element: <ProductsPage /> },
+]);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,5 @@
+function HomePage() {
+  return <h2>My Home Page</h2>;
+}
+
+export default HomePage;
