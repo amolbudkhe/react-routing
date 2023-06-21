d6ceecd374df39f8602753307403cd83814ed65c
amolbudkhe <abudkhe>
2023-06-21 12:48:56 +0530

    added other way of implementing routes commented

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ProductsPage from "./pages/ProductsPage";
 
-const router = createBrowserRouter([
-  { path: "/", element: <HomePage /> },
-  { path: "/products", element: <ProductsPage /> },
-]);
+const routeDefinations = createRoutesFromElements(
+  <Route>
+    <Route path="/" element={<HomePage />}></Route>
+    <Route path="/products" element={<ProductsPage />}></Route>
+  </Route>
+);
+
+// const router = createBrowserRouter([
+//   { path: "/", element: <HomePage /> },
+//   { path: "/products", element: <ProductsPage /> },
+// ]);
+
+const router = createBrowserRouter(routeDefinations);
 
 function App() {
   return <RouterProvider router={router} />;
